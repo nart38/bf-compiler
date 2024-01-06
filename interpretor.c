3f682d0f11884265bcec83bbdb6e4bcc441e19fb
nart38 <sezer_1966_sezgin@hotmail.com>
2024-01-06 16:45:12 +0300

    -

--- interpretor.c
+++ interpretor.c
@@ -16,8 +16,46 @@ int main(int argc, char *argv[]) {
   char *code = create_code_buff(file_dir);
   if (code == NULL)
     return 1;
-  for (int i = 0; i < strlen(code); i++) {
-    putchar(code[i]);
+  token *token_buff = create_token_buff(code);
+  if (token_buff == NULL)
+    return 1;
+
+  // Begin evaluateing token_buff
+  char mem_array[30000] = {0};
+  unsigned int mem_ptr = 0;
+  int code_len = strlen(code);
+  int i;
+
+  for (i = 0; i < code_len; i++) {
+    switch (token_buff[i].kind) {
+    case INCREASE:
+      mem_array[mem_ptr]++;
+      break;
+    case DECREASE:
+      mem_array[mem_ptr]--;
+      break;
+    case PTR_NEXT:
+      mem_ptr++;
+      break;
+    case PTR_PREV:
+      mem_ptr--;
+      break;
+    case STD_IN:
+      mem_array[mem_ptr] = getchar();
+      break;
+    case STD_OUT:
+      putchar(mem_array[mem_ptr]);
+      break;
+    case LOOP_START:
+      // Not sure but there is not much to do with LOOP_START
+      i = token_buff[i].jmp_position - 1;
+      break;
+    case LOOP_END:
+      if (mem_array[mem_ptr != 0]) {
+        i = token_buff[i].jmp_position;
+      }
+      break;
+    }
   }
-  puts("");
+  return 0;
 }
